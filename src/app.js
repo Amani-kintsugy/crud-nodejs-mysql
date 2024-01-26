33af185290abfb3cd143c02d8e01a68ee4fb908e
Amani-kintsugy <Amaninc4@gmail.com>
2024-01-26 22:00:29 +0100

    Final Fixes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import path from "path";
 import morgan from "morgan";
+import { pool } from "./db.js";
+
 
 import customerRoutes from "./routes/customer.routes.js";
 import { fileURLToPath } from "url";
@@ -16,6 +18,30 @@ app.set("view engine", "ejs");
 // middlewares
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
+
+async function checkAndCreateCustomerTable() {
+    try {
+      const [rows, fields] = await pool.query("SHOW TABLES LIKE 'customer'");
+      if (rows.length === 0) {
+        await pool.query(`
+          CREATE TABLE customer (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(50) NOT NULL,
+            address VARCHAR(100) NOT NULL,
+            phone VARCHAR(15)
+          )
+        `);
+        console.log("Customer table created successfully.");
+      } else {
+        console.log("Customer table already exists.");
+      }
+    } catch (error) {
+      console.error("Error checking or creating customer table:", error);
+    }
+  }
+  
+
+checkAndCreateCustomerTable();
 
 // routes
 app.use(customerRoutes);
