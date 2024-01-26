5380388259f46269b743e4abcf6591b9d624c319
Amani-kintsugy <Amaninc4@gmail.com>
2024-01-26 20:10:59 +0100

    Added the .env

--- src/db.js
+++ src/db.js
@@ -1,9 +1,13 @@
 import { createPool } from "mysql2/promise";
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "faztpassword",
-  port: 3306,
-  database: "customersdb",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: 3306, // Ensure port is parsed as an integer
+  database: process.env.DB_DATABASE,
 });
